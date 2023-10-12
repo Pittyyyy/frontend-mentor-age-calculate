d46dbfe62e2e9c3b4801bdbcb7e630552b4363ce
Benz <benzhomeme@gmail.com>
2023-10-12 22:00:15 +0700

    feat: final app

---
+++ src/App.jsx
@@ -0,0 +1,271 @@
+import { useState } from 'react'
+import './App.css'
+import arrowIcon from "../public/assets/images/icon-arrow.svg"
+import {monthsAndDays} from "./constants.jsx"
+import {isLeapYear} from "./function.jsx"
+
+function App() {
+  const [formData, setFormData] = useState({
+    day: "",
+    month: "",
+    year: "",
+  })
+
+  const [formErrors, setFormErrors] = useState({
+    day: "",
+    month: "",
+    year: "",
+    generic: "",
+  })
+
+  const [output, setOutput] = useState({
+    days: "",
+    months: "",
+    years: "",
+  })
+  
+  const hasErrors = formErrors.day || formErrors.month || formErrors.year || formErrors.generic;
+
+  function dateDiff(date) {
+    date = date.split('-');
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var yy = parseInt(date[0]);
+    var mm = parseInt(date[1]);
+    var dd = parseInt(date[2]);
+    var years, months, days;
+    // months
+    months = month - mm;
+    if (day < dd) {
+        months = months - 1;
+    }
+    // years
+    years = year - yy;
+    if (month * 100 + day < mm * 100 + dd) {
+        years = years - 1;
+        months = months + 12;
+    }
+    // days
+    days = Math.floor((today.getTime() - (new Date(yy + years, mm + months - 1, dd)).getTime()) / (24 * 60 * 60 * 1000));
+    //
+    return {years: years, months: months, days: days};
+
+    
+}
+
+  const handleSubmit = (day, month, year) => {
+    const dayAsNumber = Number(day);
+    const monthAsNumber = Number(month);
+    const yearAsNumber = Number(year);
+
+    const today = new Date();
+    const chosenDate = new Date(year, month -1, day);
+    const currentMonth = monthsAndDays.find(
+    (item) => item.month === monthAsNumber
+    );
+
+    const validateDaysForFebruary = () => {
+      if (monthAsNumber === 2) {
+        let maxDays;
+
+        if (isLeapYear(yearAsNumber)) {
+          maxDays = currentMonth?.days?.leap;
+        } else {
+          maxDays = currentMonth?.days?.common;
+        }
+
+        if (dayAsNumber <= maxDays) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      return false;
+    };
+
+    const isDayInputValid = dayAsNumber >= 1 && ((monthAsNumber !== 2 && dayAsNumber <= (currentMonth?.days || 31)) || validateDaysForFebruary());
+
+    const isMonthInputValid = monthAsNumber >= 1 && monthAsNumber <= 12;
+    
+    const isYearInputValid = yearAsNumber >= 1 && yearAsNumber <= today.getFullYear()
+
+    const isPastData = today - chosenDate < 0;
+
+    if (!day) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        day: "This field is required",
+        month: formErrors.month && isMonthInputValid ? "" : prevState.month,
+        year: formErrors.year && isYearInputValid ? "" : prevState.year,
+      }))
+    }
+
+    if (!month) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        day: formErrors.day && isDayInputValid ? "" : prevState.day,
+        month: "This field is required",
+        year: formErrors.year && isYearInputValid ? "" : prevState.year,
+      }))
+    }
+
+    if (!year) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        day: formErrors.day && isDayInputValid ? "" : prevState.day,
+        month: formErrors.month && isMonthInputValid ? "" : prevState.month,
+        year: "This field is required",
+      }));
+    }
+
+    const isPrecheckValid = isDayInputValid && isMonthInputValid && isYearInputValid;
+
+    if(!isPrecheckValid) {
+ 
+      if (day && !isDayInputValid) {
+        setFormErrors((prevState) => ({
+          ...prevState,
+          day: "Must be a valid day",
+          month: formErrors.month && isMonthInputValid ? "" : prevState.month,
+          year: formErrors.year && isYearInputValid ? "" : prevState.year,
+          generic: "",
+        }))
+      }
+
+      if (month && !isMonthInputValid) {
+        setFormErrors((prevState) => ({
+          ...prevState,
+          day: formErrors.day && isDayInputValid ? "" : prevState.day,
+          month: "Must be a valid month",
+          year: formErrors.year && isYearInputValid ? "" : prevState.year,
+          generic: "",
+        }))
+      }
+
+      if (year && !isYearInputValid) {
+        setFormErrors((prevState) => ({
+          ...prevState,
+          day: formErrors.day && isDayInputValid ? "" : prevState.day,
+          month: formErrors.month && isMonthInputValid ? "" : prevState.month,
+          year: "Must be a valid year",
+          generic: "",
+        }));
+      }
+    } else if (isPrecheckValid && isPastData) {
+      setFormErrors(() => ({
+        day: "",
+        month: "",
+        year: "",
+        generic: "Must be a date in the past",
+      }));
+    } else {
+      if (hasErrors) {
+        setFormErrors({
+          day: "",
+          month: "",
+          year: "",
+          generic: "",
+        });
+      }
+
+        const formattedDate = `${year}-${month}-${day}`;
+        const { years, months, days } = dateDiff(formattedDate);
+
+        setOutput({
+          days: days,
+          months: months,
+          years: years,
+        })
+      
+    }
+  
+
+  };
+
+  return (
+    <>
+<div className="card-container">
+  <div className="inputs-container">
+  <div className="input-lable-container">
+    <lable htmlFor="day" style={{ color: hasErrors ? "hsl(0, 100%, 67%)" : "hsl(0, 1%, 44%)" }}>Day</lable>
+    <input 
+    type="number" 
+    id="day" 
+    placeholder="DD" 
+    min={1} 
+    value={formData.day}
+    style={{
+      border: hasErrors ? "1px solid hsl(0, 100%, 67%)" : "1px solid hsl(0, 0%, 94%)",
+    }} 
+    onChange={(e) => setFormData((prevState) => ({...prevState, day: e.target.value}))} />
+    {formErrors.day && <p className="error">{formErrors.day}</p>}
+  </div>
+
+  <div className="input-lable-container">
+    <lable htmlFor="month" style={{ color: hasErrors ? "hsl(0, 100%, 67%)" : "hsl(0, 1%, 44%)" }}>Month</lable>
+    <input 
+    type="number" 
+    id="month" 
+    placeholder="MM" 
+    min={1} 
+    value={formData.month}
+    style={{
+      border: hasErrors ? "1px solid hsl(0, 100%, 67%)" : "1px solid hsl(0, 0%, 94%)",
+    }} 
+    onChange={(e) => setFormData((prevState) => ({...prevState, month: e.target.value}))} />
+    {formErrors.month && <p className="error">{formErrors.month}</p>}
+  </div>
+
+  <div className="input-lable-container">
+    <lable htmlFor="year" style={{ color: hasErrors ? "hsl(0, 100%, 67%)" : "hsl(0, 1%, 44%)" }}>Year</lable>
+    <input 
+    type="number" 
+    id="year" 
+    placeholder="YYYY" 
+    min={1} 
+    value={formData.year}
+    style={{
+      border: hasErrors ? "1px solid hsl(0, 100%, 67%)" : "1px solid hsl(0, 0%, 94%)",
+    }} 
+    onChange={(e) => setFormData((prevState) => ({...prevState, year: e.target.value}))} />
+    {formErrors.year && <p className="error">{formErrors.year}</p>}
+  </div>
+</div>
+
+{formErrors.generic && (<p className="error generic">{formErrors.generic}</p>)}
+
+<div className="divider-container">
+  <div className="divider"></div>
+
+  <button className="btn" onClick={() => handleSubmit(formData.day, formData.month, formData.year)}>
+    <img src={arrowIcon} alt="arrowIcon" />
+  </button>
+</div>
+  <div className="output-container">
+    <h1>
+      <span className="highlighted">
+        {output.years === "" ? '--' : output.years}{" "}
+      </span>
+    {output.years === 1 ? "year" : "years"}
+    </h1>
+    <h1>
+      <span className="highlighted">
+        {output.months === "" ? '--' : output.months}{" "}
+      </span>
+    {output.months === 1 ? "month" : "months"}
+    </h1>
+    <h1>
+      <span className="highlighted">
+        {output.days === "" ? '--' : output.days}{" "}
+      </span>
+    {output.days === 1 ? "day" : "days"}
+    </h1>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
